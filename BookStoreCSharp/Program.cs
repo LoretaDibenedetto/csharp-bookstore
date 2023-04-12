9d403a318501be0c02caa78baeeee46fd4fe9d06
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-12 14:21:11 +0200

    add more variable float and int

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,12 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+Console.WriteLine("------------------------info sul libro selezionato------------------------------");
 string title = "Clean Code";
-Console.WriteLine("il titolo del libro e' " + title);
+Console.WriteLine("il titolo del libro e':  " + title);
 
-string autor = "\r\nRobert C. Martin";
-Console.WriteLine("autore" +  autor);
+string autor = "Robert C. Martin";
+Console.WriteLine("autore: " +  autor);
 
 long ISBN = 8850334389;
-Console.WriteLine(ISBN);
+Console.WriteLine("codice ISBN: "+ ISBN);
 
+
+int pageNumber = 435;
+Console.WriteLine("numero di pagine: " + pageNumber);
+
+int weight = 860;
+Console.WriteLine("Peso del libro: " + weight + "g");
+
+float height = 17.3f;
+float lenght = 17.5f;
+float depth = 2.5f;
+Console.WriteLine("Dimensioni: " + height + " " + depth + " " + lenght + " ");
