0b0763b97e57478993bfbf608be578d40e44fba9
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-12 14:06:24 +0200

    init make variable

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+string title = "Clean Code";
+Console.WriteLine("il titolo del libro e' " + title);
+
+string autor = "\r\nRobert C. Martin";
+Console.WriteLine("autore" +  autor);
+
+long ISBN = 8850334389;
+Console.WriteLine(ISBN);
+
