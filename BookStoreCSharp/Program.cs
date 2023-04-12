db70a5bb394ba83d31809f512ecbb4c5d5dd9749
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-12 14:38:05 +0200

    add review and kindle version, flexiblecover

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -5,14 +5,14 @@ string title = "Clean Code";
 Console.WriteLine("il titolo del libro e':  " + title);
 
 string autor = "Robert C. Martin";
-Console.WriteLine("autore: " +  autor);
+Console.WriteLine("Autore: " +  autor);
 
 long ISBN = 8850334389;
-Console.WriteLine("codice ISBN: "+ ISBN);
+Console.WriteLine("Codice ISBN: "+ ISBN);
 
 
 int pageNumber = 435;
-Console.WriteLine("numero di pagine: " + pageNumber);
+Console.WriteLine("Numero di pagine: " + pageNumber);
 
 int weight = 860;
 Console.WriteLine("Peso del libro: " + weight + "g");
@@ -20,4 +20,20 @@ Console.WriteLine("Peso del libro: " + weight + "g");
 float height = 17.3f;
 float lenght = 17.5f;
 float depth = 2.5f;
-Console.WriteLine("Dimensioni: " + height + " " + depth + " " + lenght + " ");
+Console.WriteLine("Dimensioni: " + height + "cm " + depth + "cm " + lenght + "cm ");
+
+string valutation = "****";
+Console.WriteLine("Valutazione media: "+  valutation + " stelline");
+
+int rewiew = 143;
+Console.WriteLine("Numero di recensioni: " + rewiew);
+
+
+string kidleVersion = "Si";
+ 
+Console.WriteLine("E' disponibile in versione kindle? " + kidleVersion);
+
+string flexibleCover = "si";
+Console.WriteLine("E' disponibile la copertina flessibile? " + flexibleCover);
+
+Console.WriteLine("-------------------------------------------------------------------------------");
