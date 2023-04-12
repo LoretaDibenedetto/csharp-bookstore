8e0240d3f8ac0268563a779cb7ef12ad858e3326
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-12 14:59:17 +0200

    add isbn code

--- bonusBookStoreCSharp/Program.cs
+++ bonusBookStoreCSharp/Program.cs
@@ -2,5 +2,17 @@
 Console.WriteLine("inserisci titolo:");
 
 string title = Console.ReadLine();
-
 Console.WriteLine("il titolo e': " + title);
+
+Console.WriteLine("inserisci autore:");
+
+string autor =  Console.ReadLine();
+Console.WriteLine("l' autore e':" +  autor);
+
+Console.WriteLine("inserisci l'ISBN:");
+
+string ISBN = Console.ReadLine();
+int ISBNparse = int.Parse(ISBN);
+
+Console.WriteLine(ISBNparse);
+
